b585a3503726e73bc666a1bf22fa9c11495505eb
MedZg <Zghal.med1@gmail.com>
2019-08-23 15:11:30 +0100

    new commit

---
+++ EasyApp.Model/ListToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyApp.Model
+{
+   public class ListToDo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        public int StatusId { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
---
+++ EasyApp.Model/ToDo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyApp.Model
+{
+    public class ToDo
+    {
+        public ToDo()
+        {
+            ListToDos = new Collection<ListToDo>();
+        }
+        public int Id { get; set; }
+
+
+        public ICollection<ListToDo> ListToDos { get; set; }
+
+
+    }
+}
